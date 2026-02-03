502ac88770c8a40a506ac2161df4b1db3364bb85
jainil <jainiltrivedi54@gmail.com>
2026-02-03 16:43:39 +0530

    create django application url

--- learning26/students/urls.py
+++ learning26/students/urls.py
@@ -1,6 +1,6 @@
-#from django.urls import path
-#from . import views
+from django.urls import path
+from . import views
 
-#urlpatterns = [
- #  path("home/",views.studentHome),
-#]
+urlpatterns = [
+   path("home/",views.studentHome),
+]
