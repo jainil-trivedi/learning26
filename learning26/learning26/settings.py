244a7b6c03d40894c327ee1b037e5ddafbcd502b
jainil <jainiltrivedi54@gmail.com>
2026-02-03 21:49:27 +0530

    file added

--- learning26/learning26/settings.py
+++ learning26/learning26/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'learning26.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':'django.db.backends.postgresql',
+        'NAME':'learning26',
+        'USER':'postgres',
+        'PASSWORD':'122004',
+        'HOST':'localhost',
+        'PORT':'5432',
+        
+
     }
 }
 
