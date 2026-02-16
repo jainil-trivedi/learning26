3bc874448e86d63385b5095f2a15cb5081caec48
jainil <jainiltrivedi54@gmail.com>
2026-02-16 09:59:33 +0530

    update and delete added in serviceList page

--- learning26/services/urls.py
+++ learning26/services/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 urlpatterns = [
     path("serviceList/",views.servicelist,name="serviceList"),
-    path("serviceWithForm/",views.serviceWithForm,name="serviceWithForm")
+    path("serviceWithForm/",views.serviceWithForm,name="serviceWithForm"),
+    path("deleteservice/<int:id>",views.deleteservice,name = "deleteservice"),
+    path("updateservice/<int:id>",views.updateservice,name="updateservice"),
 ]
--- learning26/services/views.py
+++ learning26/services/views.py
@@ -3,7 +3,7 @@ from .models import service
 from .forms import serviceForm
 # Create your views here.
 def servicelist(request):
-    form = service.objects.all()
+    form = service.objects.all().order_by('id').values()
     return render(request,"service/serviceList.html",{'form': form})
 
 def serviceWithForm(request):
@@ -14,3 +14,17 @@ def serviceWithForm(request):
     else:
         form = serviceForm()
         return render(request,"service/serviceWithForm.html",{"form": form})
+    
+def deleteservice(request,id):
+    service.objects.filter(id=id).delete()
+    return redirect("serviceList")
+
+def updateservice(request,id):
+    Services = service.objects.get(id=id)
+    if request.method == "POST":
+        form = serviceForm(request.POST, instance=Services)
+        form.save()
+        return redirect("serviceList")
+    else:
+        form = serviceForm(instance=Services)
+        return render(request,"service/updateservice.html",{"form":form})    
